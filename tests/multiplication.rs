1bb3f67758484000ab77aee552c582b364739bd2
Tom Dean <trdean@stanford.edu>
2018-10-25 08:11:05 -0700

    Adds degree 32 test

--- tests/multiplication.rs
+++ tests/multiplication.rs
@@ -73,3 +73,37 @@ fn degree8_mul() {
         println!("\n\n");
     }
 }
+
+#[test]
+fn degree32_mul() {
+    let mut rng = thread_rng();
+
+    let mut p1 = Polynomial::zeros( 32 );
+    let mut p2 = Polynomial::zeros( 32 );
+
+    for i in 0 .. 20 {
+        println!("Test {}", i );
+        for i in 0 .. 32 {
+            p1[i] = rng.gen_range( -30, 30 );
+            p2[i] = rng.gen_range( -30, 30 );
+        }
+        println!("p1 = {}", p1 );
+        println!("p2 = {}", p2 );
+
+        let plan_maybe = KaratsubaPlan::<i32>::new_plan( 32 );
+
+        let mut ka = Polynomial::with_capacity( 48 );
+
+        if let Ok(mut plan) = plan_maybe {
+            assert!( plan.execute_plan( &mut ka, &p1, &p2 ).is_ok() );
+            let gs = p1.gradeschool_mul( &p2 ); 
+
+            println!("Gradeschool: {}", gs );
+            println!("Karatsuba: {}", ka );
+            assert!( ka == gs );
+        } else {
+            assert!(false);
+        }
+        println!("\n\n");
+    }
+}
